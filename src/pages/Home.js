d56008d70d5b2bc60b5f090d5e0e8931a5f37f82
shendo7 <rodneyrjames@gmail.com>
2019-09-19 13:18:40 -0700

    calc update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,87 +1,109 @@
 import React from "react";
 import Heading from "../components/Jumbotron";
-import {
-  Container,
-  Row,
-  Col
-} from 'reactstrap';
+import { Container, Row, Col } from "reactstrap";
 
 function Home() {
   return (
-    <div style={{
-      backgroundImage: `url(https://images.wallpaperscraft.com/image/cube_dark_texture_shape_119956_300x168.jpg)`,
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
-      backgroundSize: "100%",
-      backgroundAttachment: "fixed"
-    }}>
+    <div
+      style={{
+        backgroundImage: `url(https://images.wallpaperscraft.com/image/cube_dark_texture_shape_119956_300x168.jpg)`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "100%",
+        backgroundAttachment: "fixed"
+      }}
+    >
       <Heading backgroundImage="https://cdn.pixabay.com/photo/2018/12/02/10/07/web-3850917__480.jpg">
-        <h1 style={{
-          fontWeight: "bold",
-          color: "white"
-        }}>Home</h1>
-        <h2 style={{
-          color: "white"
-        }}>Your journey starts here!</h2>
+        <h1
+          style={{
+            fontWeight: "bold",
+            color: "White"
+          }}
+        >
+          CryptoCurrency Mining
+        </h1>
+        <h2
+          style={{
+            color: "White"
+          }}
+        >
+          Your journey starts here!
+        </h2>
         <br />
         <br />
       </Heading>
-      <Container style={{ marginTop: 30 }}>>
+      <Container style={{ marginTop: 30 }}>
+        >
         <Row>
-          <Col size="md-12"
+          <Col
+            size="md-12"
             style={{
               color: "white"
-            }}>
+            }}
+          >
             <h1>Welcome To Crypto Mining!</h1>
             <hr />
           </Col>
         </Row>
         <Row>
-          <Col size="md-12"
+          <Col
+            size="md-12"
             style={{
               color: "white"
-            }}>
+            }}
+          >
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
-              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
-              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
-              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
-              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
-              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
-              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
-              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
-              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
-              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
-              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              aliquet diam tortor, id consequat mauris ullamcorper eu. Orci
+              varius natoque penatibus et magnis dis parturient montes, nascetur
+              ridiculus mus. Pellentesque et dui id justo finibus sollicitudin
+              at et metus. Ut feugiat tellus nec metus commodo, sed suscipit
+              nisi gravida. Duis eget vestibulum quam, ut porttitor sem. Donec
+              sagittis mi sollicitudin turpis semper, et interdum risus
+              lobortis. Vestibulum suscipit nunc non egestas tristique. Proin
+              hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
+              quam non, tristique tempor erat. Nullam non sem facilisis, tempus
+              tortor sit amet, volutpat nisl. Ut et turpis non nunc maximus
+              mollis a vitae tortor. Pellentesque mattis risus ac quam laoreet
+              cursus. Praesent suscipit orci neque, vestibulum tincidunt augue
+              tincidunt non. Duis consequat mattis tortor vitae mattis.
             </p>
             <p>
-              Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
-              malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
-              ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
-              nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
-              Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
-              porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
+              Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum
+              porttitor. Sed malesuada molestie velit ac viverra. Quisque a
+              ullamcorper purus. Curabitur luctus mi ac mi hendrerit semper.
+              Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
+              nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue
+              dictum egestas. Aliquam blandit mi eu urna scelerisque, vitae
+              volutpat ligula ultricies. Maecenas vel porta augue. Fusce mauris
+              ex, dignissim et lacinia ut, tempus eget nibh.
             </p>
             <p>
-              Etiam ut massa efficitur, gravida sapien non, condimentum sapien. Suspendisse massa
-              tortor, facilisis in neque sit amet, scelerisque elementum tortor. Nullam eget nibh
-              sit amet odio lobortis ullamcorper. Nulla bibendum magna nec sem pulvinar lobortis.
-              Mauris et imperdiet urna, vitae lobortis dui. Nunc elementum elit mi, non mattis enim
-              congue at. Proin mi lectus, ullamcorper id hendrerit eu, ultricies vitae lacus. Nunc
-              vehicula, erat eget laoreet condimentum, felis ante malesuada leo, nec efficitur diam
-              nisi eget nisi. Cras arcu lacus, tristique in bibendum vitae, elementum eget lorem.
-              Maecenas vestibulum volutpat orci eu pharetra. Praesent vel blandit ante, nec faucibus
-              libero. Sed ultrices lorem ex, eu facilisis libero convallis ac. Vivamus id dapibus
-              eros. Nullam tempor sem rhoncus porta semper. Proin bibendum vulputate nisl, fringilla
-              interdum elit pulvinar eu. Quisque vitae quam dapibus, vestibulum mauris quis, laoreet
-              massa.
+              Etiam ut massa efficitur, gravida sapien non, condimentum sapien.
+              Suspendisse massa tortor, facilisis in neque sit amet, scelerisque
+              elementum tortor. Nullam eget nibh sit amet odio lobortis
+              ullamcorper. Nulla bibendum magna nec sem pulvinar lobortis.
+              Mauris et imperdiet urna, vitae lobortis dui. Nunc elementum elit
+              mi, non mattis enim congue at. Proin mi lectus, ullamcorper id
+              hendrerit eu, ultricies vitae lacus. Nunc vehicula, erat eget
+              laoreet condimentum, felis ante malesuada leo, nec efficitur diam
+              nisi eget nisi. Cras arcu lacus, tristique in bibendum vitae,
+              elementum eget lorem. Maecenas vestibulum volutpat orci eu
+              pharetra. Praesent vel blandit ante, nec faucibus libero. Sed
+              ultrices lorem ex, eu facilisis libero convallis ac. Vivamus id
+              dapibus eros. Nullam tempor sem rhoncus porta semper. Proin
+              bibendum vulputate nisl, fringilla interdum elit pulvinar eu.
+              Quisque vitae quam dapibus, vestibulum mauris quis, laoreet massa.
             </p>
           </Col>
         </Row>
       </Container>
-      <div className="footer" style={{
-        padding: "30px"
-      }}></div>
+      <div
+        className="footer"
+        style={{
+          padding: "30px"
+        }}
+      ></div>
     </div>
   );
 }
