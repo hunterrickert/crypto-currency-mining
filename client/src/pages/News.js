272edca680c8872aacb2337ffed5e0db51f5f418
Tim Sherman <tim.rs.sherman@gmail.com>
2019-09-23 10:10:25 -0700

    merging

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -1,43 +1,23 @@
 import React, { Component } from "react";
-import API from "../utils/API";
 import {
   Container
 } from 'reactstrap';
-import SearchForm from "../components/SearchForm";
-import SearchResults from "../components/SearchResults";
-import Alert from "../components/Alert";
+// import SearchResults from "../components/SearchResults";
 import Heading from "../components/Jumbotron";
+
+
 
 class News extends Component {
   state = {
-    search: "",
-    breeds: [],
-    results: [],
-    error: ""
+    articles: []
   };
 
-  // When the component mounts, get a list of all available base breeds and update this.state.breeds
-  componentDidMount() {
-    API.getBaseBreedsList()
-      .then(res => this.setState({ breeds: res.data.message }))
-      .catch(err => console.log(err));
-  }
+  // componentDidMount() {
+  //   ProxyApi.getTopNewsByCoin("bitcoin")
+  //   .then(res => this.setState({ articles: res }))
+  //   .catch(err => console.log(err));
+  // }
 
-  handleInputChange = event => {
-    this.setState({ search: event.target.value });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    API.getDogsOfBreed(this.state.search)
-      .then(res => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
-        this.setState({ results: res.data.message, error: "" });
-      })
-      .catch(err => this.setState({ error: err.message }));
-  };
   render() {
     return (
       <div>
@@ -53,19 +33,9 @@ class News extends Component {
           <br />
         </Heading>
         <Container>
-          <h1 className="text-center">Search By Breed!</h1>
-          <Alert
-            type="danger"
-            style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
-          >
-            {this.state.error}
-          </Alert>
-          <SearchForm
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-            breeds={this.state.breeds}
-          />
-          <SearchResults results={this.state.results} />
+          <h1>Top News on Bitcoin:</h1>
+          <br />
+          {/* <SearchResults articles={this.state.articles} /> */}
         </Container>
       </div>
     );
