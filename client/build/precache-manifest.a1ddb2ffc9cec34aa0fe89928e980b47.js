8493049038ccc8ee9ff36185e4beed893d4baf7c
Hunter Rickert <hunter.rickert@gmail.com>
2019-09-24 13:12:39 -0700

    changing scripts around

---
+++ client/build/precache-manifest.a1ddb2ffc9cec34aa0fe89928e980b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0841fc3e0ae088f01c656d17c7bfbd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c56da7744b334bb5594f",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c56da7744b334bb5594f",
+    "url": "/static/js/2.9679b989.chunk.js"
+  },
+  {
+    "revision": "4d94a364abf01c27e28b",
+    "url": "/static/js/main.dc793a9b.chunk.js"
+  },
+  {
+    "revision": "dc96cabb973fedae4117",
+    "url": "/static/js/runtime~main.f790af40.js"
+  },
+  {
+    "revision": "75e5c985f7a592316c9e149ff49985ac",
+    "url": "/static/media/bitcoinimage.75e5c985.jpg"
+  },
+  {
+    "revision": "0b6b35d0444019c90bf386c080c30dc9",
+    "url": "/static/media/ethereum.0b6b35d0.jpg"
+  }
+]);
