27005fd796b345503203560e70d84cb87cf2a0d0
Hunter Rickert <hunter.rickert@gmail.com>
2019-09-25 10:49:08 -0700

    austin

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ require("./services/passport");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+// }
 // Add routes, both API and view
 app.use(routes);
 app.get("/info", function(req, res) {
